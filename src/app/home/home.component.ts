82db7ccf6d9a66b53dac3ce8565d23c1e8f4746a
DEV-HUSTLING <anushkareddy@anushkas-mbp.mynetworksettings.com>
2024-06-11 14:20:24 -0400

    Made basic layout for home page on to the about page

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavComponent } from '../component/nav/nav.component';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NavComponent],
+  styleUrls: ['./home.component.css'], // Use styleUrls for multiple style files
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+  imagePath: string = '/assets/Frame_logo.png';
+
+}
